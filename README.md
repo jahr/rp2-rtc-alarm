61f7ec593c3b90ac0625fb94457d16e0678bcf57
Jan Hrudka <jahr@atlas.cz>
2025-08-05 16:55:13 +0200

    Updated for aon_timer.h

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 >Simple micropython wrapper to RP Pico hardware RTC Alarm
 
 
-This module gives access to Pico SDK's RTC Alarm functions.
+This module gives access to Pico SDK's RTC Alarm functions. It is tested only on RP2040.
+it's tested on Pico W with MicroPython v1.26.0-preview.524 with Pico-SDK 2.1.1
 
 >Build:
 
 go to ports/rp2 directory and run
 
 ```
-make BOARD=PICO_W USER_C_MODULES=../../../rp2-rtc-alarm/rp2_rtc_alarm.cmake
+make BOARD=RPI_PICO_W USER_C_MODULES=../../../rp2-rtc-alarm/rp2_rtc_alarm.cmake
 ```
 
 (of course adjust the BOARD and rp2-rtc-alarm directory location)
@@ -17,12 +18,12 @@ make BOARD=PICO_W USER_C_MODULES=../../../rp2-rtc-alarm/rp2_rtc_alarm.cmake
 In the micropython the module rtcalarm is available and has following functions:
 
 ```
-set_alarm(datetime, callback) - datetime is tuple with 7 integers, callback is function with no arguments
-enable_alarm() - starts alarm
+set_alarm() - 
+enable_alarm(datetime, callback) - starts alarm. datetime is tuple with 8 integers (the last one is ignored), callback is function with no arguments
 disable_alarm() - stops alarm
 ```
 
-datetime tuple contains `(year, month, day, dayofweek, hour, minute, second)` telling when the alarm will be fired and any of them may be replaced with -1, which means that it is not checked. Eg. alarm firing every minute at 0 sec. should be set with `(-1, -1, -1, -1, -1, -1, 0)`.
+datetime tuple contains `(year, month, day, dayofweek, hour, minute, second, ignored)` telling when the alarm will be fired and any of them may be replaced with -1, which means that it is not checked. Eg. alarm firing every minute at 0 sec. should be set with `(-1, -1, -1, -1, -1, -1, 0, 0)`.
 
 >Simple example code:
 
@@ -34,12 +35,12 @@ import rtcalarm as ra
 rtc = RTC()
 
 def show_time(_):
-  _, _, _, _, hour, minute, _, _ = rtc.datetime()
-  print('{:2d}:{:02d}'.format(hour, minute))
+  _, _, _, _, hour, minute, second, _ = rtc.datetime()
+  print('{:2d}:{:02d}:{:02d}'.format(hour, minute, second))
 
 def rtcalarm_callback():
   mp.schedule(show_time, None)
 
-ra.set_alarm((-1, -1, -1, -1, -1, -1, 0), rtcalarm_callback)
+ra.enable_alarm((-1, -1, -1, -1, -1, -1, 0, 0), rtcalarm_callback)
 ```
 
--- rp2_rtc_alarm.c
+++ rp2_rtc_alarm.c
@@ -1,12 +1,9 @@
-#include <stdio.h>
-#include <string.h>
+#include <time.h>
 
-#include "py/nlr.h"
 #include "py/obj.h"
 #include "py/runtime.h"
-#include "py/mphal.h"
-#include "hardware/rtc.h"
-#include "pico/util/datetime.h"
+
+#include "pico/aon_timer.h"
 
 static mp_obj_t callback;
 static void callback_func() {
@@ -16,39 +13,32 @@ static void callback_func() {
     mp_call_function_0(callback);
 }
 
-static mp_obj_t rp2_rtc_alarm_set_alarm(mp_obj_t dt, mp_obj_t cb) {
+static mp_obj_t rp2_rtc_alarm_enable_alarm(mp_obj_t dt, mp_obj_t cb) {
     mp_obj_t *items;
-    mp_obj_get_array_fixed_n(dt, 7, &items);
-    datetime_t t = {
-        .year = mp_obj_get_int(items[0]),
-        .month = mp_obj_get_int(items[1]),
-        .day = mp_obj_get_int(items[2]),
-        .dotw = mp_obj_get_int(items[3]),
-        .hour = mp_obj_get_int(items[4]),
-        .min = mp_obj_get_int(items[5]),
-        .sec = mp_obj_get_int(items[6]),
+    mp_obj_get_array_fixed_n(dt, 8, &items);
+    struct tm t = {
+        .tm_year = mp_obj_get_int(items[0]) - 1900,
+        .tm_mon  = mp_obj_get_int(items[1]) - 1,
+        .tm_mday = mp_obj_get_int(items[2]),
+        .tm_wday = mp_obj_get_int(items[3]),
+        .tm_hour = mp_obj_get_int(items[4]),
+        .tm_min  = mp_obj_get_int(items[5]),
+        .tm_sec  = mp_obj_get_int(items[6]),
     };
     callback = cb;
-    rtc_set_alarm(&t, callback_func);
+    aon_timer_enable_alarm_calendar(&t, callback_func, 0);
     return mp_const_none;
 }
-static MP_DEFINE_CONST_FUN_OBJ_2(rp2_rtc_alarm_set_alarm_obj, rp2_rtc_alarm_set_alarm);
-
-static mp_obj_t rp2_rtc_alarm_enable_alarm() {
-    rtc_enable_alarm();
-    return mp_const_none;
-}
-static MP_DEFINE_CONST_FUN_OBJ_0(rp2_rtc_alarm_enable_alarm_obj, rp2_rtc_alarm_enable_alarm);
+static MP_DEFINE_CONST_FUN_OBJ_2(rp2_rtc_alarm_enable_alarm_obj, rp2_rtc_alarm_enable_alarm);
 
 static mp_obj_t rp2_rtc_alarm_disable_alarm() {
-    rtc_disable_alarm();
+    aon_timer_disable_alarm();
     return mp_const_none;
 }
 static MP_DEFINE_CONST_FUN_OBJ_0(rp2_rtc_alarm_disable_alarm_obj, rp2_rtc_alarm_disable_alarm);
 
 static const mp_rom_map_elem_t rp2_rtc_alarm_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_rtcalarm) },
-    { MP_ROM_QSTR(MP_QSTR_set_alarm), MP_ROM_PTR(&rp2_rtc_alarm_set_alarm_obj) },
     { MP_ROM_QSTR(MP_QSTR_enable_alarm), MP_ROM_PTR(&rp2_rtc_alarm_enable_alarm_obj) },
     { MP_ROM_QSTR(MP_QSTR_disable_alarm), MP_ROM_PTR(&rp2_rtc_alarm_disable_alarm_obj) },
 };
